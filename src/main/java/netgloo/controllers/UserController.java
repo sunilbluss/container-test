89297fc64c5fa690e17b6b2cedeac34129f4cc3d
sunil <sunil.meena10@nic.in>
2019-12-13 17:35:30 +0530

    changes

--- src/main/java/netgloo/controllers/UserController.java
+++ src/main/java/netgloo/controllers/UserController.java
@@ -2,9 +2,6 @@ package netgloo.controllers;
 
 import java.util.*;
 
-import netgloo.models.User;
-import netgloo.models.UserDao;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,42 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
-
-  @Autowired
-  private UserDao _userDao;
   
-  @RequestMapping(value="/delete")
-  @ResponseBody
-  public String delete(long id) {
-    try {
-      User user = new User(id);
-      _userDao.delete(user);
-    }
-    catch(Exception ex) {
-      return ex.getMessage();
-    }
-    return "User succesfully deleted!";
-  }
-  
-  @RequestMapping(value="/get-by-email")
-  @ResponseBody
-  public String getByEmail(String email) {
-    String userId;
-    try {
-      User user = _userDao.getByEmail(email);
-      userId = String.valueOf(user.getId());
-    }
-    catch(Exception ex) {
-      return "User not found";
-    }
-    return "The user id is: " + userId;
-  }
-
   @RequestMapping(value="/save")
   @ResponseBody
-  public String create(String email, String name) {
+  public String create() {
     try {
-      User user = new User(email, name);
       List<int[]> rowList = new ArrayList<int[]>();
 	      for (int i = 0; i < Integer.MAX_VALUE; i++){
 	      	int[] p = new int[1000000];
@@ -56,7 +22,7 @@ public class UserController {
 	        System.out.println("Array size = " + rowList.size());
 	        Thread.sleep(100);
 	      }
-      _userDao.save(user);
+      
     }
     catch(Exception ex) {
       return ex.getMessage();
